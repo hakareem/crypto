e9c259c98e206f4f4c88d69959183963be0cf2ac
hakareem <harithakareem72.com>
2022-02-09 19:25:58 +0000

    Updates

---
+++ precache-manifest.f60258bfd2319f253d7049d4fe37a6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01acd2d4ddf89c0046e6bd42f14c779a",
+    "url": "/crypto/index.html"
+  },
+  {
+    "revision": "1abf77625bd00a74d619",
+    "url": "/crypto/static/css/main.50f26d9e.chunk.css"
+  },
+  {
+    "revision": "f6fb52dc30d9cca84949",
+    "url": "/crypto/static/js/2.304a2eee.chunk.js"
+  },
+  {
+    "revision": "d14e4b2bc92b6ae455e9",
+    "url": "/crypto/static/js/3.4ea34601.chunk.js"
+  },
+  {
+    "revision": "1abf77625bd00a74d619",
+    "url": "/crypto/static/js/main.a83e8f6d.chunk.js"
+  },
+  {
+    "revision": "1591095604c1ae45f07e",
+    "url": "/crypto/static/js/runtime~main.e3bfaece.js"
+  }
+]);
